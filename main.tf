550b29a5990638ab88090c761d53eb8e127aaffa
Jai Karve <jai.karve@gmail.com>
2021-12-14 16:23:29 -0600

    Second commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     databricks = {
       source = "databrickslabs/databricks"
-      version = "0.4.0"
+      version = "~> 0.4.0"
     }
     azuread = {
       source = "hashicorp/azuread"
